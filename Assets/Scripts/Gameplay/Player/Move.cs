60b34a9f32b18028ee98d79bb4c190440af4a816
Alec <movieornasasim@gmail.com>
2023-01-07 20:18:53 -0600

    Updated Move to store player facing direction
    Updated player movement script to store a normalized Vector3 containing the direction the player is facing.

--- Assets/Scripts/Gameplay/Player/Move.cs
+++ Assets/Scripts/Gameplay/Player/Move.cs
@@ -6,12 +6,13 @@ public class Move : MonoBehaviour
 {
     CharacterController _controller;
     [SerializeField] float Speed = 7;
-
+    public Vector3 FacingDirection {get; private set;}
     void Awake() => _controller = GetComponent<CharacterController>();
     void MoveCharacter()
     {
         float xAxis = Input.GetAxis("Horizontal");
         Vector3 MoveVector = new Vector3(Time.deltaTime * xAxis * Speed, 0, 0);
+        FacingDirection = MoveVector.normalized;
         _controller.Move(MoveVector);
     }
 
