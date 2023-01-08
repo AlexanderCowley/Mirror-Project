5bdfc82af35c3405d3a94cfc928402e2607179d6
Alec <movieornasasim@gmail.com>
2023-01-08 16:14:21 -0600

    Added toggle to ignore player interactions
    Added a toggle for the Interact script for direct interactions with the player.
    The OnInteract method signature takes a boolean called isPlayer to determine if the player is interacting with the object in question.

--- Assets/Scripts/Gameplay/Interaction/Interact.cs
+++ Assets/Scripts/Gameplay/Interaction/Interact.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Interact : MonoBehaviour
 {
+    [field: SerializeField] public bool IsInteractable {get; private set;} = true;
     public Action InteractHandler;
     void Awake() 
     {
         GetEvents();
     }
-    
-    public void OnInteract() => InteractHandler?.Invoke();
+
+    public void OnInteract(bool isPlayer)
+    {
+        if(!IsInteractable && isPlayer)
+            return;
+        
+        InteractHandler?.Invoke();
+    }
 
     void GetEvents()
     {
