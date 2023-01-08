085d59dd3a66da76fda5de8a9a8f8c4968cbfd1f
Alec <movieornasasim@gmail.com>
2023-01-07 20:32:52 -0600

    Player interact raycast reads facing direction
    On PlayerInteract, the raycast for both the gizmos and the raycast itself now reads the player's facing direction.

--- Assets/Scripts/Gameplay/Player/PlayerInteract.cs
+++ Assets/Scripts/Gameplay/Player/PlayerInteract.cs
@@ -3,12 +3,18 @@ public class PlayerInteract : MonoBehaviour
 {
     Transform _rayCastTransform;
     [SerializeField] float InteractRange = 1.2f;
+    Move _playerMovement;
+    Vector3 FacingDirection;
 
-    void Awake() => _rayCastTransform = transform.GetChild(0).transform;
+    void Awake()
+    {
+        _rayCastTransform = transform.GetChild(0).transform;
+        _playerMovement = GetComponent<Move>();
+    } 
     void Interact()
     {
         RaycastHit hit;
-        if(Physics.Raycast(_rayCastTransform.position, Vector3.right, out hit, InteractRange))
+        if(Physics.Raycast(_rayCastTransform.position, FacingDirection, out hit, InteractRange))
             hit.transform.GetComponent<Interact>()?.OnInteract();
     }
 
@@ -19,12 +25,15 @@ public class PlayerInteract : MonoBehaviour
             
         //DrawRay does not have a max length parameter so just multiply it by direction
         Gizmos.color = Color.cyan;
-        Gizmos.DrawRay(_rayCastTransform.position, Vector3.right * InteractRange);
+        Gizmos.DrawRay(_rayCastTransform.position, FacingDirection * InteractRange);
     }
 
     void Update() 
     {
         if(Input.GetKeyDown(KeyCode.E))
             Interact();
+
+        if(_playerMovement != null)
+            FacingDirection = _playerMovement.FacingDirection;
     }
 }
