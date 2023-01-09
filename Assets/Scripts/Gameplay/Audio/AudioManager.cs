9296f7e938219d01da6dd5a3f0c636d09c4c85cc
Alec <movieornasasim@gmail.com>
2023-01-09 10:41:41 -0600

    Created AudioManager to handle audio
    AudioManager can play background music and can play individual audio clips based on the AudioSource.

---
+++ Assets/Scripts/Gameplay/Audio/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get{return _instance;}
+
+        private set{ _instance = value; }
+    }
+
+    AudioSource _bgAudioSource;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        else
+            Instance = this;
+
+        _bgAudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySceneBG()
+    {
+        PlayMusic(_bgAudioSource);
+    }
+
+    public void PlayMusic(AudioSource audioSource)
+    {
+        audioSource?.Play();
+    }
+
+    public void SetBGVolume(float volumeLevel, float volumeBlendDuration)
+    {
+
+    }
+}
