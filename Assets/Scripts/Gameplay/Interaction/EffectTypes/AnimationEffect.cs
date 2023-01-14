fe541e35f6506e0188b5b5f67e6bcfc3e6873e58
Alec <movieornasasim@gmail.com>
2023-01-14 16:18:39 -0600

    Enables one time animation upon interacting
    Updated AnimationEffect to play an animation specified by a serialized string.
    Added RequireComponent attribute to ensure the animator component is present.
    Checks for invalid animation name.

--- Assets/Scripts/Gameplay/Interaction/EffectTypes/AnimationEffect.cs
+++ Assets/Scripts/Gameplay/Interaction/EffectTypes/AnimationEffect.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class AnimationEffect : InteractEffect
 {
     Animator _animator;
-
+    [SerializeField] string AnimName = string.Empty;
+    static int _animStateHash;
+    bool _invalidStateName = false;
     void Awake() 
     {
         _animator = GetComponent<Animator>();
+        if(AnimName == string.Empty)
+        {
+            Debug.LogError("The Animation State Name has not been filled out!", gameObject);
+            _invalidStateName = true;
+            return;
+        }
+
+        _animStateHash = Animator.StringToHash(AnimName);
     }
     
     public override void InteractEvent()
@@ -16,6 +27,9 @@ public class AnimationEffect : InteractEffect
 
     void PlayAnimation()
     {
-        Debug.Log("Play Anim Here");
+        if(_invalidStateName)
+            return;
+
+        _animator.CrossFade(_animStateHash, 0f, 0);
     }
 }
